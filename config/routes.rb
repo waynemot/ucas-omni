9803f313319a32e1146d308878429f7dee83181a
Wayne Motycka <wmotycka@cse.unl.edu>
2019-08-05 10:02:34 -0500

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ Rails.application.routes.draw do
   #get 'sessions/failure'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
+  get '/foo', to: 'contents#index'
   get '/login', to: 'sessions#new', as: :login
-  get '/content', to: 'content#index'
+  get '/contents', to: 'contents#index'
+  #get '/auth/cas/callback', to: 'contents#index'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/auth/failure', to: 'sessions#failure', via: [:get, :post]
+  match '/auth/logout', to: 'sessions#logout', via: [:delete]
   root to: 'contents#index'
 end
